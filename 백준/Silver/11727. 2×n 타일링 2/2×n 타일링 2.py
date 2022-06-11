c056dc72590f2b2ba8cf2aa56ea485b0856bcd2a
Kim Joon Su <padomay1352@gmail.com>
2022-06-12 08:32:04 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 88 ms, Memory: 32952 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,9 @@
+from math import factorial
+import sys
+n = int(sys.stdin.readline())
+dp = [0 for i in range(n+1)]
+dp[0] = 1
+dp[1] = 1
+for i in range(2,n+1):
+    dp[i] = (dp[i-1] + (dp[i-2]*2))%10007
+print(dp[n])
