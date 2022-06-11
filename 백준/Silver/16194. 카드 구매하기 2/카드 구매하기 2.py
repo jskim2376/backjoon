96fb3fb992a95c175da4ddd589748f11566f5fac
Kim Joon Su <padomay1352@gmail.com>
2022-06-12 08:32:33 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 276 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,12 @@
+import sys
+card_count = int(sys.stdin.readline())
+cardpack = list(map(int, sys.stdin.readline().split()))
+dp = [0 for i in range(card_count)]
+dp[0] = cardpack[0]
+dp[1] = min(cardpack[0]+cardpack[0], cardpack[1])
+
+for i in range(2,card_count):
+    dp[i] = cardpack[i]
+    for j in range(0,i):
+        dp[i] = min(dp[i], dp[j]+dp[i-j-1])
+print(dp[card_count-1])
