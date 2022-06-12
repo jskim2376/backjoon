2ed21f31f46ac476949947bd85c3013d1a52e45d
Kim Joon Su <padomay1352@gmail.com>
2022-06-12 12:50:21 +0900

    [Silver III] Title: 쇠막대기, Time: 100 ms, Memory: 30852 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,18 +1,15 @@
 import sys
 line = sys.stdin.readline()[:-1]
 
-i=0
-result = 0
 stack = []
-while i<len(line):
-    if i+1<len(line) and line[i]=="(" and line[i+1]==")":
-        result+=len(stack)
-        i+=2
-    elif line[i]=="(":
+result = 0
+for i in range(len(line)):
+    if line[i]=="(":
         stack.append('(')
-        i+=1
     elif line[i]==")":
         stack.pop()
-        i+=1
-        result+=1
+        if line[i-1]==")":
+            result+=1
+        else:
+            result+=len(stack)
 print(result)        
