94adb4d65275444f88fcfbd633c40f8c77417c84
Kim Joon Su <padomay1352@gmail.com>
2023-07-02 23:54:43 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,13 @@
+def checkPrime(n):
+    if n==1:
+        return 0
+    for i in range(2, n//2+1):
+        if n % i == 0:
+            return 0
+    return 1
+count = 0
+input()
+for n in list(map(int, input().split())):
+    count+=checkPrime(n)
+
+print(count)
